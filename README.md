78ac053d5920e26240d9fbca5004b77c24a8ae76
Ben Lu <ben@ben-l.com>
2017-09-21 13:08:36 +1000

    Updated article links

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 Server Side Rendering with Create React App
 ===========================================
 
-This is the v2 version of my react app, checkout my article here: https://medium.com/@benlu/ssr-with-create-react-app-v2-1b8b520681d9
+This is the v2.1 version of my react app, checkout my article here: 
+https://medium.com/@benlu/ssr-with-create-react-app-v2-1-ee83fb767327
 
 Install
 -------
--- src/containers/App.js
+++ src/containers/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
       <div>
         <h1>Server Side Rendering with Create React App v2.1</h1>
         <p>This code is on github: <a href='https://github.com/ayroblu/ssr-cra-v2.1'>https://github.com/ayroblu/ssr-cra-v2.1</a></p>
-        <p>Medium article: <a href='https://medium.com/@benlu/ssr-with-create-react-app-v2-1b8b520681d9'>https://medium.com/@benlu/ssr-with-create-react-app-v2-1b8b520681d9</a></p>
+        <p>Medium article: <a href='https://medium.com/@benlu/ssr-with-create-react-app-v2-1-ee83fb767327'>https://medium.com/@benlu/ssr-with-create-react-app-v2-1-ee83fb767327</a></p>
         <Switch>
           <Route exact path="/" component={FirstPage}/>
           <Route path="/second" component={SecondPage}/>
